8efebddf18458f62dd0ae623e152c66c596d4b00
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-01-08 23:20:31 +0000

    Project Euler Start

--- ProjectEuler/euler1.hs
+++ ProjectEuler/euler1.hs
@@ -3,4 +3,10 @@
 sumMults3or5 :: Int 
 sumMults3or5 = sum [n | n <- [1..1000] , n `mod` 5 == 0 || n `mod` 3 == 0]
 
+--Problem 2
 
+fibs :: [Int]
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+sum' :: Int 
+sum' = sum $ filter even (takeWhile (<4000000) fibs) 
